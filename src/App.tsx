5c33e29ec9effb19c8be904016de06e2db22ef19
zein <173869188+gitzein@users.noreply.github.com>
2025-07-14 23:51:20 +0700

    Metero shower & blur text added

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,18 @@
 import Aurora from "./components/Aurora/Aurora";
+import BlurText from "./components/BlurText/BlurText";
+import MeteorShower from "./components/MeteorShower";
 import ShinyText from "./components/ShinyText/ShinyText";
 import Technologies from "./components/technologies/Technologies";
 import useTheme from "./hooks/useTheme";
 import { cn } from "./lib/utils";
 
 function App() {
-  const [theme, setTheme] = useTheme();
+  const [theme] = useTheme();
 
   return (
     <main
       className={cn(
-        `relative w-full bg-[#0a0a0a] bg-[url(/assets/stardust.png)] text-slate-900 dark:text-neutral-100`,
+        `relative w-full overflow-hidden bg-[#08000a] bg-[url(/assets/stardust.png)] text-slate-900 dark:text-neutral-100`,
         {
           dark: theme === "dark",
         },
@@ -22,28 +24,41 @@ function App() {
         blend={1}
         colorStops={["#8aed07", "#0e392f", "#743c55"]}
       />
-      <div className="flex w-full flex-col justify-center gap-8">
-        <ShinyText
+      <MeteorShower />
+      <div className="absolute bottom-0 left-[50%] z-0 h-full w-[140vw] -translate-x-[50%] bg-radial-[at_50%_100%] from-violet-200/50 via-violet-400/30 via-5% to-60%" />
+      <div className="relative !z-50 flex w-full flex-col items-center justify-center gap-8">
+        <BlurText
           text="Zein Farhan Makarim"
           className="text-center text-6xl font-bold"
         />
-        <div className="relative mx-auto flex size-5 items-center justify-center">
+
+        {/* <div className="relative mx-auto flex size-5 items-center justify-center">
           <span className="absolute inline-flex h-full w-full animate-[ping_2.5s_cubic-bezier(0,_0,_0.2,_1)_infinite] rounded-full bg-neutral-100 opacity-75 duration-100"></span>
           <span className="relative inline-flex size-2 rounded-full bg-neutral-200"></span>
+        </div> */}
+        {/* <h2 className="text-center text-4xl font-bold">
+          Self taught developer
+        </h2> */}
+        <div className="text-center">
+          <p>Front-end Developer</p>
+          <p>Bekasi, Indonesia</p>
         </div>
-        {/* <h2 className="text-center text-4xl font-bold">Self taught developer</h2> */}
         {/* <button
-        onClick={() => {
-          setTheme(theme === "dark" ? "light" : "dark");
-        }}
-        className="cursor-pointer bg-black p-2 text-white"
-        >
-        toggle theme
-      </button> */}
+          onClick={() => {
+            setTheme(theme === "dark" ? "light" : "dark");
+          }}
+          className="cursor-pointer bg-black p-2 text-white"
+          >
+          toggle theme
+        </button> */}
         <ShinyText
           text="Technologies"
           className="text-center text-5xl font-bold"
         />
+        <Technologies />
+
+        <ShinyText text="Projects" className="text-center text-5xl font-bold" />
+        <Technologies />
         <Technologies />
       </div>
     </main>
---
+++ src/components/MeteorShower.tsx
@@ -0,0 +1,17 @@
+function MeteorShower() {
+  return (
+    <div className="star-container !z-0">
+      <div className="star" />
+      <div className="star" />
+      <div className="star" />
+      <div className="star" />
+      <div className="star" />
+      <div className="star" />
+      <div className="star" />
+      <div className="star" />
+      <div className="star" />
+      <div className="star" />
+    </div>
+  );
+}
+export default MeteorShower;
